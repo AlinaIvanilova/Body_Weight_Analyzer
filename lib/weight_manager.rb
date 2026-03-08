ba130315ac195fa5f53826c2d776f162365b5282
AlinaIvanilova <alinatelf@gmail.com>
2026-03-08 10:01:11 +0200

    version 2.1

--- lib/weight_manager.rb
+++ lib/weight_manager.rb
@@ -1,22 +1,26 @@
 require 'json'
 
-class Weight_Manager
+class WeightManager
   FILE_PATH = "data/weights.json"
 
   def load_weights
     return [] unless File.exist?(FILE_PATH)
 
     file = File.read(FILE_PATH)
-    data = JSON.parse(file)
+    JSON.parse(file)
   end
+
+
+
 
   def save_weights(weights)
     File.write(FILE_PATH, JSON.pretty_generate(weights))
   end
 
-  def add_weights(weights, weight)
+  def add_weight(weights, weight)
     entry = {
-      date: Time.now.strftime("%Y-%m-%d"),
+      "date" => Time.now.strftime("%Y-%m-%d"),
+      "weight" => weight
     }
 
     weights << entry
--- main.rb
+++ main.rb
@@ -3,8 +3,7 @@ require_relative 'lib/weight_manager'
 require_relative 'lib/analyzer'
 require_relative 'lib/chart'
 
-
-manager = Weight_Manager.new
+manager = WeightManager.new
 weights = manager.load_weights
 
 loop do
@@ -21,23 +20,23 @@ loop do
     print 'Введіть вашу вагу: '
     weight = gets.chomp.to_f
 
-    manager.add_weights(weights, weight)
+    manager.add_weight(weights, weight)
     puts "Вага збережена!"
 
   when '2'
     if weights.empty?
-      puts "Історія порожня?"
+      puts "Історія порожня"
     else
       weights.each do |entry|
-        puts "#{entry["date"]} - #{entry["weight"]}"
+        puts "#{entry["date"]} - #{entry["weight"]} кг"
       end
     end
 
   when '3'
     puts 'До побачення!'
     break
-    
-  else 
+
+  else
     puts 'Невірний вибір!'
   end
 end
